946f03ea4af253320b607a0926ca296d5a4827a7
antipov.ma <antipov.ma@sberleasing.ru>
2020-01-24 14:51:57 +0300

    build

---
+++ precache-manifest.8851aaf73802d0bcd347f0fc8a8a0264.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f75275b81174fc0c0d8",
+    "url": "infinity-news/css/chunk-7b7d6b8c.abf9df1a.css"
+  },
+  {
+    "revision": "c2b765a4d969193f52db",
+    "url": "infinity-news/css/chunk-vendors.c641d84c.css"
+  },
+  {
+    "revision": "793a3aa069072301895a790fd591e82e",
+    "url": "infinity-news/index.html"
+  },
+  {
+    "revision": "54496fbb3fe35b7476ee",
+    "url": "infinity-news/js/app.634f6b1d.js"
+  },
+  {
+    "revision": "9f75275b81174fc0c0d8",
+    "url": "infinity-news/js/chunk-7b7d6b8c.4e7dc056.js"
+  },
+  {
+    "revision": "c2b765a4d969193f52db",
+    "url": "infinity-news/js/chunk-vendors.8e659a15.js"
+  },
+  {
+    "revision": "e57e7c00dc33bcd437b1d544837d4585",
+    "url": "infinity-news/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "infinity-news/robots.txt"
+  }
+]);
